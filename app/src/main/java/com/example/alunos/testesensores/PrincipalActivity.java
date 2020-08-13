792f4b8d5a7168ccb9c47e009590310382ab9339
rodrigoqi <rodrigo.barreto@qi.edu.br>
2020-08-13 09:07:18 -0300

    Versão inicial do projeto de sensores com os xml das telas já disponibilizados

---
+++ app/src/main/java/com/example/alunos/testesensores/PrincipalActivity.java
@@ -0,0 +1,15 @@
+package com.example.alunos.testesensores;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+
+    }
+
+}
---
+++ app/src/main/java/com/example/alunos/testesensores/SensoresActivity.java
@@ -0,0 +1,16 @@
+package com.example.alunos.testesensores;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class SensoresActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensores);
+
+    }
+
+}
